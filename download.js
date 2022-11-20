d0349adddc4ce7e147977a5f7e544914af349466
bigmurtaza <muratcansarkalkan@gmail.com>
2022-11-20 19:59:26 +0300

    Initial commit

---
+++ download.js
@@ -0,0 +1,59 @@
+// Equivalent to Python's OS
+const fs = require('fs')
+const testFolder = './test/';
+const { google } = require('googleapis')
+
+const filerealId = '1NobeewSi5KG-AejPotWky7e22egEUWT_'
+
+const GOOGLE_API_FOLDER_ID = '19O6WJS6QKnLnmP9I_q-bNORcV490hkNi'
+
+const http = require('http'); // or 'https' for https:// URLs
+
+async function downloadFile(filerealId){
+    try{
+        
+        // https://googleapis.dev/nodejs/googleapis/latest/
+        const auth = new google.auth.GoogleAuth({
+            keyFile: './googlekey.json',
+            scopes: [
+                'https://www.googleapis.com/auth/drive',
+                'https://www.googleapis.com/auth/drive.appdata',
+                'https://www.googleapis.com/auth/drive.file',
+                'https://www.googleapis.com/auth/drive.metadata',
+                'https://www.googleapis.com/auth/drive.metadata.readonly',
+                'https://www.googleapis.com/auth/drive.photos.readonly',
+                'https://www.googleapis.com/auth/drive.readonly',
+            ]
+        })
+
+        const drive = google.drive({version: 'v3', auth})
+
+        // https://googleapis.dev/nodejs/googleapis/latest/drive/classes/Resource$Files.html#get
+        const file = await drive.files.get({
+            fileId: filerealId,
+            alt: 'media',
+          }, {responseType: "stream"});
+
+        const metadata = drive.files.get({ fileId: filerealId, fields: '*' });
+
+        metadata.then(function(result) {
+            resultnew = result.data.name;
+            const localFile = fs.createWriteStream(resultnew);
+            file.data
+              .on("end", () => {
+                 console.log("Done");
+              })
+              .on("error", err => {
+                 console.log("Error", err);
+              })
+              .pipe(localFile);
+        })
+    
+        return file.status;
+    } catch (err) {
+        // TODO(developer) - Handle error
+        throw err;
+    }
+}
+
+downloadFile(filerealId)
---
+++ upload.js
@@ -0,0 +1,50 @@
+// Equivalent to Python's OS
+const fs = require('fs')
+const testFolder = './test/';
+const { google } = require('googleapis')
+
+const filerealId = '1NobeewSi5KG-AejPotWky7e22egEUWT_'
+
+const GOOGLE_API_FOLDER_ID = '19O6WJS6QKnLnmP9I_q-bNORcV490hkNi'
+
+async function uploadFile(file){
+    try{
+        let newfile = (testFolder+file).split('/')
+        
+        const auth = new google.auth.GoogleAuth({
+            keyFile: './googlekey.json',
+            scopes: ['https://www.googleapis.com/auth/drive']
+        })
+
+        const driveService = google.drive({version: 'v3', auth})
+
+        const fileMetaData = {
+            'name': newfile[newfile.length - 1],
+            'parents': [GOOGLE_API_FOLDER_ID]
+        }
+
+        const media = {
+            mimeType: 'text/plain',
+            body: fs.createReadStream(file)
+        }
+
+        const response = await driveService.files.create({
+            resource: fileMetaData,
+            media: media,
+            field: 'id'
+        })
+        return response.data.id
+
+    }catch(err){
+        console.log('Upload file error', err)
+    }
+}
+
+fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+        uploadFile(testFolder+file).then(data => {
+            console.log(data)
+            //https://drive.google.com/uc?export=view&id=
+        })
+    });
+});
